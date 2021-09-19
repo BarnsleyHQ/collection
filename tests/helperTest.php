19c02c70b7a03b0534c9c8b9670d9e491f21d427
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2021-09-19 19:19:56 +0100

    test: helper file

---
+++ tests/helperTest.php
@@ -0,0 +1,14 @@
+<?php
+
+use AlexBarnsley\Collection;
+use PHPUnit\Framework\TestCase;
+
+class helperTest extends TestCase
+{
+    public function testCollectMethod()
+    {
+        $this->assertTrue(function_exists('collect'));
+        $this->assertEquals(['test1', 'test2'], collect(['test1', 'test2'])->toArray());
+        $this->assertEquals(['test1'], collect('test1')->toArray());
+    }
+}
